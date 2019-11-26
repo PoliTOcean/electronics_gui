73b1b37b7e88c0c266c9999ab8e2c0c52c52f9c2
pettinz <angelo.pettinelli@icloud.com>
2019-11-26 14:04:40 +0100

    loginRoute

--- lib/helpers/router.dart
+++ lib/helpers/router.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 class Router {
   static const String homeRoute = '/';
+  static const String loginRoute = '/login';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case homeRoute:
         return MaterialPageRoute(builder: (context) => Container());
-
+      case loginRoute:
+        return MaterialPageRoute(builder: (context) => Container());
       default:
         return null;
     }
