551ab6fb07141ee6fc046244d362124ba204ea0d
Angelo Pettinelli <angelo.pettinelli@studenti.polito.it>
2019-11-28 18:19:09 +0100

    Added authentication blocs

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'blocs/blocs.dart';
 
 import 'helpers/helpers.dart';
+import 'resources/resources.dart';
+import 'views/views.dart';
 
 class App extends StatelessWidget {
+  final UserRepository _userRepository;
+
+  App({Key key, @required UserRepository userRepository})
+      : _userRepository = userRepository,
+        super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       onGenerateRoute: Router.generateRoute,
-      initialRoute: Router.homeRoute,
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state is Uninitialized) {
+            return LoginPage();
+          } else if (state is Authenticated) {
+            return Container();
+          } else {
+            return null;
+          }
+        },
+      ),
     );
   }
 }
