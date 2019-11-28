a7eeccd161fae99bc12786d2791116e19d6cb469
Angelo Pettinelli <angelo.pettinelli@studenti.polito.it>
2019-11-28 09:57:06 +0100

    Updated to last user model

--- lib/models/user/user.g.dart
+++ lib/models/user/user.g.dart
@@ -8,12 +8,18 @@ part of 'user.dart';
 
 User _$UserFromJson(Map<String, dynamic> json) {
   return User(
-    studentId: json['_id'] as String,
+    studentId: json['_id'] as int,
     password: json['password'] as String,
+    firstName: json['firstName'] as String,
+    lastName: json['lastName'] as String,
+    permissions: json['permissions'] as int,
   );
 }
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       '_id': instance.studentId,
       'password': instance.password,
+      'firstName': instance.firstName,
+      'lastName': instance.lastName,
+      'permissions': instance.permissions,
     };
