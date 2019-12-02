acb28c597f2e8489229d9dee84802a2583944d0e
Angelo Pettinelli <angelo.pettinelli@studenti.polito.it>
2019-12-02 12:36:13 +0100

    Updated with login page and added add component route

--- lib/helpers/router.dart
+++ lib/helpers/router.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
-// import '../views/views.dart';
+import '../views/views.dart';
 
 class Router {
   static const String homeRoute = '/';
   static const String loginRoute = '/login';
+  static const String addRoute = '/add';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      /*
       case homeRoute:
         return MaterialPageRoute(builder: (context) => HomePage());
       case loginRoute:
+        return MaterialPageRoute(
+            builder: (context) =>
+                LoginPage(userRepository: settings.arguments));
+      case addRoute:
         return MaterialPageRoute(builder: (context) => Container());
-      */
       default:
         return null;
     }
