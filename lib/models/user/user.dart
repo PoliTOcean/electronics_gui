968a949137adb256f5ca7a688ef8fbdf894170e4
Angelo Pettinelli <angelo.pettinelli@studenti.polito.it>
2019-12-04 14:48:35 +0100

    Updated

--- lib/models/user/user.dart
+++ lib/models/user/user.dart
@@ -11,7 +11,7 @@ class User extends Equatable {
   final String email;
   final String firstName;
   final String lastName;
-  final int privileges;
+  final String privileges;
 
   User(
       {@required this.userId,
